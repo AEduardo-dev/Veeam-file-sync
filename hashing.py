647ab3dc58bc73ec2f4d93269acc644625b74e2b
Angel Eduardo Vega Conde <56698379+AEduardo-dev@users.noreply.github.com>
2023-07-26 09:54:20 +0200

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -9,7 +9,7 @@ def hash_file(file_path: str, buf_size: int = 65536) -> Any:
     # BUF_SIZE is totally arbitrary, change for your app!
     if buf_size <= 0:
         LOGGER.fatal(
-            f"Impossible to parse file with buffer size smaller than 1 bit. Please check your settings and try again."
+            f"Impossible to parse file with buffer size smaller than 1 bit. Please check your settings and try again. Buffer_size = {buf_size}"
         )
 
     md5 = hashlib.md5()
